b0d134a2b44dbc0ba2f788439cb6b2ab4d7fa63a
Qixin Hu <qixinhu98@gmail.com>
2024-05-09 20:40:21 +0800

    remove 0

--- my_segmentation/ProstateSeg/get_moe_results.py
+++ my_segmentation/ProstateSeg/get_moe_results.py
@@ -25,11 +25,11 @@ def get_dice(args):
 
     dice_list = []
     for item in test_list:
-        pred0 = np.load(os.path.join(pred_root0, item+'.npz'))['pred'] # 224*224*30
+        # pred0 = np.load(os.path.join(pred_root0, item+'.npz'))['pred'] # 224*224*30
         pred1 = np.load(os.path.join(pred_root1, item+'.npz'))['pred'] # 224*224*30
         pred2 = np.load(os.path.join(pred_root2, item+'.npz'))['pred'] # 224*224*30
         pred3 = np.load(os.path.join(pred_root3, item+'.npz'))['pred'] # 224*224*30
-        pred = ((pred0 + pred1 + pred2 + pred3) > 0).astype(np.float32) # 224*224*30
+        pred = ((pred1 + pred2 + pred3) > 0).astype(np.float32) # 224*224*30
         label = np.load(os.path.join(label_root, item+'.npz'))['mask'][args.modality,...].astype(np.float32) # 224*224*30
 
         current_dice = cal_dice(pred, label)
