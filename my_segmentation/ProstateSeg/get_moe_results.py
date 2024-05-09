1cf0080d452bbd1445492631a9fb2736bbf487d5
Qixin Hu <qixinhu98@gmail.com>
2024-05-09 20:42:47 +0800

    only pred3

--- my_segmentation/ProstateSeg/get_moe_results.py
+++ my_segmentation/ProstateSeg/get_moe_results.py
@@ -29,7 +29,7 @@ def get_dice(args):
         pred1 = np.load(os.path.join(pred_root1, item+'.npz'))['pred'] # 224*224*30
         pred2 = np.load(os.path.join(pred_root2, item+'.npz'))['pred'] # 224*224*30
         pred3 = np.load(os.path.join(pred_root3, item+'.npz'))['pred'] # 224*224*30
-        pred = ((pred1 + pred3) > 0).astype(np.float32) # 224*224*30
+        pred = (( pred3) > 0).astype(np.float32) # 224*224*30
         label = np.load(os.path.join(label_root, item+'.npz'))['mask'][args.modality,...].astype(np.float32) # 224*224*30
 
         current_dice = cal_dice(pred, label)
