8238dbc5e69dcf1e9f1f5f086608fcc732fadbae
Qixin Hu <qixinhu98@gmail.com>
2024-05-31 22:31:46 +0800

    add classification

--- classification/vote_for_resutls.py
+++ classification/vote_for_resutls.py
@@ -9,7 +9,7 @@ warnings.filterwarnings("ignore")
 
 
 def _get_data_loader(modality):
-    file_root = "../data/preprocess"
+    file_root = "/Users/qixinhu/Project/CUHK/Prostate/PAIsData/0426/qixin/CLS"
     file_list = sorted(os.listdir(os.path.join(file_root, modality)))
             
     test_images = []
@@ -25,50 +25,20 @@ def _get_data_loader(modality):
     return test_loader
 
 
-def _get_val_loader(args):
-    file_root = args.file_root
-    labels_dict = np.load(os.path.join(file_root,"case_level_label.npz"), allow_pickle=True)['label'].item()
-    val_samples = []
-    with open(os.path.join(file_root,'test_case_level.txt'), 'r') as f:
-        lines = f.readlines()
-        for line in lines:
-            sample = line.strip()
-            val_samples.append(sample)
-        
-    val_images = []
-    val_labels = []
-    for sample in val_samples:
-        val_images.append(os.path.join(file_root, args.modality, sample + '.npz'))
-        val_labels.append(labels_dict[sample])
-    val_labels = np.array(val_labels, dtype=float)
-    val_labels = torch.FloatTensor(val_labels)  
-
-
-    val_img_transform = transforms.Compose(
-        [
-            transforms.EnsureChannelFirst(channel_dim="no_channel"),
-            # transforms.Resize(spatial_size=(x, y, z), mode="area"),
-        ]
-    )
-
-    
-    val_ds = prostateDataset(npz_files=val_images, labels=val_labels, img_transforms=val_img_transform, seg_transforms=None)
-    val_loader = DataLoader(val_ds, batch_size=1, shuffle=False,num_workers=0, pin_memory=True)
-
-    return val_loader
-
-
 def test_one_model(model, test_loader):
     model.eval()
     name_list = []
     y_pred = []
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     with torch.no_grad():
         for test_data in test_loader:
             test_name = test_data['name'][0].split('/')[-1].split('.')[0]
-            test_images  = test_data['image'].to("cuda")
+            test_images  = test_data['image'].to(device)
             test_preds = model(test_images)
+            test_preds = torch.sigmoid(test_preds)[0,1]
             test_preds  = test_preds.detach().cpu().numpy()
-            y_pred.append(np.argmax(test_preds))
+            # y_pred.append(np.argmax(test_preds))
+            y_pred.append(test_preds.item())
             name_list.append(test_name)
 
     return name_list, y_pred
@@ -79,7 +49,7 @@ def voting_models():
     from models.resnet import generate_model
     n_classes = 2
     model_depth = 18
-
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     all_preds = {}
     preds = []
     for modality in ["T2W", "DWI", "ADC", "T2W_ADC", "T2W_DWI", "DWI_ADC", "ALL"]:
@@ -93,9 +63,9 @@ def voting_models():
             
         model = generate_model(model_depth=model_depth,
                                 input_H=128, input_W=128, input_D=input_D, n_classes=n_classes)
-        model.cuda()
+        model.to(device)
         # loading model
-        model.load_state_dict(torch.load(f"runs/{modality}/resnet18/model_best.pt"))
+        model.load_state_dict(torch.load(f"runs/{modality}/resnet18/model_best.pt", map_location=device))
         # exam the model
         print("-" * 20)
         print(f"Start to Evaluation {modality}")
@@ -120,7 +90,7 @@ def voting_models():
         rows.append(row)
 
     # save metrics to cvs file
-    csv_name = '../classification_prediction.csv'
+    csv_name = './classification_prediction.csv'
     with open(csv_name, 'w', encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(header)
